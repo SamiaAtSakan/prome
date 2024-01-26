8e68009ac685a2de713fb8bfb12e9962f49d2c7d
fawad kaleem <fwdkaleem@gmail.com>
2024-01-26 11:01:09 +0500

    event changes

--- lib/profilepages/events/create_event.dart
+++ lib/profilepages/events/create_event.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:prome/apis/create_event_apii.dart';
@@ -9,10 +8,10 @@ import 'package:prome/utils/textformfield.dart';
 import 'package:intl/intl.dart';
 
 class CreateEvent extends StatefulWidget {
-  const CreateEvent({super.key});
+  const CreateEvent({Key? key}) : super(key: key);
 
   @override
-  State<CreateEvent> createState() => _CreateEventState();
+  _CreateEventState createState() => _CreateEventState();
 }
 
 class _CreateEventState extends State<CreateEvent> {
@@ -28,15 +27,16 @@ class _CreateEventState extends State<CreateEvent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Create Event",
-            style: TextStyle(color: backgroundColor),
-          ),
+      appBar: AppBar(
+        title: Text(
+          "Create Event",
+          style: TextStyle(color: backgroundColor),
         ),
-        body: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Center(
               child: Stack(
                 children: [
@@ -49,14 +49,16 @@ class _CreateEventState extends State<CreateEvent> {
                               'https://static.remove.bg/remove-bg-web/a6eefcd21dff1bbc2448264c32f7b48d7380cb17/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png'),
                         ),
                   Positioned(
-                      bottom: -10,
-                      left: 70,
-                      child: IconButton(
-                          onPressed: () => selectImage(),
-                          icon: Icon(
-                            Icons.add_a_photo,
-                            color: Colors.black,
-                          )))
+                    bottom: -10,
+                    left: 70,
+                    child: IconButton(
+                      onPressed: () => selectImage(),
+                      icon: Icon(
+                        Icons.add_a_photo,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -97,38 +99,12 @@ class _CreateEventState extends State<CreateEvent> {
                           controller: _eventStartDate,
                           hintText: "Enter Date",
                           textInputType: TextInputType.datetime,
-                          onTap: () async {
-                            DateTime? pickedDate = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(
-                                    2000), //DateTime.now() - not to allow to choose before today.
-                                lastDate: DateTime(2101));
-
-                            if (pickedDate != null) {
-                              print(
-                                  pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                              String formattedDate =
-                                  DateFormat('yyyy-MM-dd').format(pickedDate);
-                              print(
-                                  formattedDate); //formatted date output using intl package =>  2021-03-16
-                              //you can implement different kind of Date Format here according to your requirement
-
-                              setState(() {
-                                _eventStartDate.text =
-                                    formattedDate; //set output date to TextField value.
-                              });
-                            } else {
-                              print("Date is not selected");
-                            }
-                          },
-                        )
+                          onTap: () => pickDate(_eventStartDate),
+                        ),
                       ],
                     ),
                   ),
-                  const SizedBox(
-                    width: 5,
-                  ),
+                  const SizedBox(width: 5),
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -138,35 +114,11 @@ class _CreateEventState extends State<CreateEvent> {
                           controller: _eventEndDate,
                           hintText: "Enter Date",
                           textInputType: TextInputType.datetime,
-                          onTap: () async {
-                            DateTime? pickedDate = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime(
-                                    2000), //DateTime.now() - not to allow to choose before today.
-                                lastDate: DateTime(2101));
-
-                            if (pickedDate != null) {
-                              print(
-                                  pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                              String formattedDate =
-                                  DateFormat('yyyy-MM-dd').format(pickedDate);
-                              print(
-                                  formattedDate); //formatted date output using intl package =>  2021-03-16
-                              //you can implement different kind of Date Format here according to your requirement
-
-                              setState(() {
-                                _eventEndDate.text =
-                                    formattedDate; //set output date to TextField value.
-                              });
-                            } else {
-                              print("Date is not selected");
-                            }
-                          },
-                        )
+                          onTap: () => pickDate(_eventEndDate),
+                        ),
                       ],
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
@@ -183,47 +135,12 @@ class _CreateEventState extends State<CreateEvent> {
                           controller: _eventStartTime,
                           hintText: "Enter Time",
                           textInputType: TextInputType.datetime,
-                          onTap: () async {
-                            TimeOfDay? pickedTime = await showTimePicker(
-                              initialTime: TimeOfDay.now(),
-                              context: context,
-                            );
-
-                            if (pickedTime != null) {
-                              print(
-                                  pickedTime.format(context)); //output 10:51 PM
-
-                              // Create a DateTime object with today's date and the picked time
-                              DateTime parsedTime = DateTime(
-                                DateTime.now().year,
-                                DateTime.now().month,
-                                DateTime.now().day,
-                                pickedTime.hour,
-                                pickedTime.minute,
-                              );
-
-                              print(
-                                  parsedTime); //output 2024-01-26 22:53:00.000
-
-                              String formattedTime =
-                                  DateFormat('hh:mm a').format(parsedTime);
-                              print(formattedTime); //output 22:53:00
-
-                              setState(() {
-                                _eventStartTime.text =
-                                    formattedTime; //set the value of text field.
-                              });
-                            } else {
-                              print("Time is not selected");
-                            }
-                          },
-                        )
+                          onTap: () => pickTime(_eventStartTime),
+                        ),
                       ],
                     ),
                   ),
-                  const SizedBox(
-                    width: 6,
-                  ),
+                  const SizedBox(width: 6),
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -233,41 +150,8 @@ class _CreateEventState extends State<CreateEvent> {
                           controller: _eventEndTime,
                           hintText: "Enter Time",
                           textInputType: TextInputType.datetime,
-                          onTap: () async {
-                            TimeOfDay? pickedTime = await showTimePicker(
-                              initialTime: TimeOfDay.now(),
-                              context: context,
-                            );
-
-                            if (pickedTime != null) {
-                              print(
-                                  pickedTime.format(context)); //output 10:51 PM
-
-                              // Create a DateTime object with today's date and the picked time
-                              DateTime parsedTime = DateTime(
-                                DateTime.now().year,
-                                DateTime.now().month,
-                                DateTime.now().day,
-                                pickedTime.hour,
-                                pickedTime.minute,
-                              );
-
-                              print(
-                                  parsedTime); //output 2024-01-26 22:53:00.000
-
-                              String formattedTime =
-                                  DateFormat('hh:mm a').format(parsedTime);
-                              print(formattedTime); //output 22:53:00
-
-                              setState(() {
-                                _eventEndTime.text =
-                                    formattedTime; //set the value of text field.
-                              });
-                            } else {
-                              print("Time is not selected");
-                            }
-                          },
-                        )
+                          onTap: () => pickTime(_eventEndTime),
+                        ),
                       ],
                     ),
                   ),
@@ -276,30 +160,9 @@ class _CreateEventState extends State<CreateEvent> {
             ),
             Center(
               child: ElevatedButton(
-                onPressed: () async {
-                  if (event_name.text.isEmpty) {
-                    messageBar("Event Name is Required", context);
-                  } else if (_eventtDescription.text.isEmpty) {
-                    messageBar("Event Description is Required", context);
-                  } else if (_eventLocation.text.isEmpty) {
-                    messageBar("Event Location is Required", context);
-                  } else if (_eventStartDate.text.isEmpty ||
-                      _eventEndDate.text.isEmpty) {
-                    messageBar("Both Start and End Date is Required", context);
-                  } else if (_eventStartTime.text.isEmpty ||
-                      _eventEndTime.text.isEmpty) {
-                    messageBar("Both Start and End Time is Required", context);
-                  } else {
-                    EventApi().createEventApi(
-                        event_name.text,
-                        _eventtDescription.text,
-                        _eventLocation.text,
-                        _eventStartDate.text,
-                        _eventEndTime.text,
-                        _eventStartTime.text,
-                        _eventEndTime.text,
-                        _image,
-                        context);
+                onPressed: () {
+                  if (validateFields()) {
+                    createEvent();
                   }
                 },
                 child: Text(
@@ -307,19 +170,92 @@ class _CreateEventState extends State<CreateEvent> {
                   style: TextStyle(color: white),
                 ),
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: backgroundColor, fixedSize: Size(325, 60)),
+                  backgroundColor: backgroundColor,
+                  fixedSize: Size(325, 60),
+                ),
               ),
             ),
-            const SizedBox(
-              height: 40,
-            ),
-          ]),
-        ));
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
   }
 
-  //Functions
+  void pickDate(TextEditingController controller) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
 
-  selectImage() async {
+    if (pickedDate != null) {
+      String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+      setState(() {
+        controller.text = formattedDate;
+      });
+    }
+  }
+
+  void pickTime(TextEditingController controller) async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      initialTime: TimeOfDay.now(),
+      context: context,
+    );
+
+    if (pickedTime != null) {
+      DateTime parsedTime = DateTime(
+        DateTime.now().year,
+        DateTime.now().month,
+        DateTime.now().day,
+        pickedTime.hour,
+        pickedTime.minute,
+      );
+
+      String formattedTime = DateFormat('hh:mm a').format(parsedTime);
+      setState(() {
+        controller.text = formattedTime;
+      });
+    }
+  }
+
+  bool validateFields() {
+    if (event_name.text.isEmpty ||
+        _eventtDescription.text.isEmpty ||
+        _eventLocation.text.isEmpty ||
+        _eventStartDate.text.isEmpty ||
+        _eventEndDate.text.isEmpty ||
+        _eventStartTime.text.isEmpty ||
+        _eventEndTime.text.isEmpty) {
+      messageBar("Please fill in all required fields.", context);
+      return false;
+    }
+    return true;
+  }
+
+  void createEvent() async {
+    try {
+      EventApi().createEventApi(
+        event_name.text,
+        _eventtDescription.text,
+        _eventLocation.text,
+        _eventStartDate.text,
+        _eventEndDate.text,
+        _eventStartTime.text,
+        _eventEndTime.text,
+        _image,
+        context,
+      );
+
+      // Optionally, you can navigate to a different screen after successful event creation.
+    } catch (error) {
+      print('Failed to create event: $error');
+      messageBar("Failed to create event. Please try again.", context);
+    }
+  }
+
+  void selectImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
