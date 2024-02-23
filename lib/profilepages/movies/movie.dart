bc7aeb83bbff2721debccf9755d80d7650bb49c2
fawad kaleem <fwdkaleem@gmail.com>
2024-02-23 22:19:46 +0500

    Movies

--- lib/profilepages/movies/movie.dart
+++ lib/profilepages/movies/movie.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:onboarding/onboarding.dart';
+import 'package:prome/apis/movies_api.dart';
 
 class Movies extends StatefulWidget {
   const Movies({super.key});
@@ -9,41 +10,23 @@ class Movies extends StatefulWidget {
 }
 
 class _MoviesState extends State<Movies> {
+  bool isMovie = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [TextButton(onPressed: () {}, child: Text("Filters"))],
-        title: Text(
-          "Movies",
-          style: TextStyle(color: background),
+        appBar: AppBar(
+          actions: [
+            TextButton(
+                onPressed: () {
+                  MoviesAPi().getMovieApi();
+                },
+                child: Text("Filters"))
+          ],
+          title: Text(
+            "Movies",
+            style: TextStyle(color: background),
+          ),
         ),
-      ),
-      body: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 2 / 3,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          itemBuilder: (BuildContext ctx, index) {
-            return GridTile(
-              footer: GridTileBar(
-                backgroundColor: Colors.black54,
-                title: Text(
-                  'Movie Name',
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text("5 Months ago"),
-                trailing: const Icon(Icons.add),
-              ),
-              child: Image.network(
-                'https://static.vecteezy.com/system/resources/previews/019/896/008/non_2x/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png',
-                fit: BoxFit.cover,
-              ),
-            );
-          }),
-    );
+        body: Text("Movies are Loaded"));
   }
 }
